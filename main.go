b3532be13a59c343ff3ca409ef98f68ff17a91c4
Shelby Diamond <shelbydiamond518@gmail.com>
2020-03-09 10:23:51 -0700

    adding flag function

--- main.go
+++ main.go
@@ -6,6 +6,8 @@
 // https://books.google.com/books?id=HW-5SZ1HKusC&pg=PA345&lpg=PA345&dq=what+is+%258d+in+go&source=bl&ots=Frb016T1jt&sig=ACfU3U0Sl5P4qnELMtqls9rmhizgoGdAOw&hl=en&sa=X&ved=2ahUKEwibuM3YooboAhVqHzQIHYP8AakQ6AEwAnoECAYQAQ#v=onepage&q=what%20is%20%258d%20in%20go&f=false
 // https://golang.org/pkg/fmt/
 // https://stackoverflow.com/questions/24562942/golang-how-do-i-determine-the-number-of-lines-in-a-file-efficiently
+// https://gobyexample.com/command-line-flags
+// https://www.digitalocean.com/community/tutorials/how-to-use-the-flag-package-in-go
 
 package main
 
@@ -17,7 +19,6 @@ import (
 )
 
 func main() {
-
 	// https://golang.org/pkg/os/
 	// Any argument typed AFTER the you list your arguments get passed into the os.Args array with the name of the program at the first index
 
