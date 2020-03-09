ad1d47ddfd8f2a224274cad453a7b064bff69606
Shelby Diamond <shelbydiamond518@gmail.com>
2020-03-09 11:13:08 -0700

    attempting to create the -flag

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
 // Resources I used in case I need to look back on it:
+
 // https://osquery.readthedocs.io/en/1.4.7/installation/cli-flags/
 // https://amehta.github.io/posts/2019/03/wc-implementation-in-go-lang/
 // https://golang.org/pkg/os/
@@ -8,14 +9,15 @@
 // https://stackoverflow.com/questions/24562942/golang-how-do-i-determine-the-number-of-lines-in-a-file-efficiently
 // https://gobyexample.com/command-line-flags
 // https://www.digitalocean.com/community/tutorials/how-to-use-the-flag-package-in-go
+// https://golang.org/cmd/compile/
+// https://en.wikipedia.org/wiki/Wc_(Unix)
+// https://www.digitalocean.com/community/tutorials/how-to-use-the-flag-package-in-go
 
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -30,39 +32,4 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Once you have the name of the file, you can open it with the os.Open function. If it fails to open, it will give a basic reason why.
-	// https://pkg.go.dev/os?tab=doc
-
-	file, error := os.Open(fileName)
-	if error != nil {
-		fmt.Println("Error ", error)
-	}
-
-	//  This code is going to scan the file line by line and print the output to the console using the bufio package.
-
-	scanner := bufio.NewScanner(file)
-
-	// sets the number of lines, words, and characters to 0 to start
-
-	lines, words, characters := 0, 0, 0
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-
-		// The total number of characters will be the length of each line we iterate through while reading the file and
-		lines++
-
-		line := scanner.Text()
-		characters += len(line)
-
-		// In order to get the number of words we need to split the line based on when the spacebar was used and count the total number of splits.
-
-		splitLines := strings.Split(line, " ")
-		words += len(splitLines)
-	}
-
-	// now that we have the answer, we need to display it to the console
-	// %d = 1 for each lines, words, and characters
-	// %s = will show the string
-
-	fmt.Printf("lines: %d\nWords: %d\nCharacters: %d\nFile Name Read: %s \n", lines, words, characters, fileName)
 }
