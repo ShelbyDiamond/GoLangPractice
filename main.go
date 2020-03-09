fae0e4e53000ca68d6c9a773b9f4530ecd9cb0c9
Shelby Diamond <shelbydiamond518@gmail.com>
2020-03-09 15:19:53 -0700

    back to the original. Attempt to add the -l flag; take 2

--- main.go
+++ main.go
@@ -12,24 +12,61 @@
 // https://golang.org/cmd/compile/
 // https://en.wikipedia.org/wiki/Wc_(Unix)
 // https://www.digitalocean.com/community/tutorials/how-to-use-the-flag-package-in-go
+// https://golang.org/pkg/flag/
+// https://www.youtube.com/watch?v=v_8584-jm7I
+// https://flaviocopes.com/go-command-line-flags/
 
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
-	// https://golang.org/pkg/os/
-	// Any argument typed AFTER the you list your arguments get passed into the os.Args array with the name of the program at the first index
-
-	var fileName string
-	if len(os.Args) > 1 {
-		fileName = os.Args[1]
+	// https://golang.org/pkg/os/		// https://golang.org/pkg/os/
+	// Any argument typed AFTER the you list your arguments get passed into the os.Args array with the name of the program at the first index		// Any argument typed AFTER the you list your arguments get passed into the os.Args array with the name of the program at the first index
+	var fileName string	
+	if len(os.Args) > 1 {	
+		fileName = os.Args[1]	
 	} else {
-		fmt.Println("I'm sorry, your file is missing. Try running `go run <current_file> <file_name>` for better results")
+		fmt.Println("I'm sorry, your file is missing. Try running `go run <current_file> <file_name>` for better results")			fmt.Println("I'm sorry, your file is missing. Try running `go run <current_file> <file_name>` for better results")
 		os.Exit(1)
 	}
+	// Once you have the name of the file, you can open it with the os.Open function. If it fails to open, it will give a basic reason why.	
+	// https://pkg.go.dev/os?tab=doc	
 
+	file, error := os.Open(fileName)	
+	if error != nil {	
+		fmt.Println("Error ", error)	
+	}	
+
+	//  This code is going to scan the file line by line and print the output to the console using the bufio package.	
+
+	scanner := bufio.NewScanner(file)	
+
+	// sets the number of lines, words, and characters to 0 to start	
+
+	lines, words, characters := 0, 0, 0	
+	for scanner.Scan() {	
+		fmt.Println(scanner.Text())	
+
+		// The total number of characters will be the length of each line we iterate through while reading the file and	
+		lines++	
+
+		line := scanner.Text()	
+		characters += len(line)	
+
+		// In order to get the number of words we need to split the line based on when the spacebar was used and count the total number of splits.	
+
+		splitLines := strings.Split(line, " ")	
+		words += len(splitLines)	
+	}	
+
+	// now that we have the answer, we need to display it to the console	
+	// %d = 1 for each lines, words, and characters	
+	// %s = will show the string	
+
+	fmt.Printf("lines: %d\nWords: %d\nCharacters: %d\nFile Name Read: %s \n", lines, words, characters, fileName)
 }
